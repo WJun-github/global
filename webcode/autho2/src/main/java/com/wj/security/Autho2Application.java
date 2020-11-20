8e206d8e119686fa10a623115c35df06dd008434
wj <wangjun104@fiberhome.com>
2020-11-20 17:21:25 +0800

    修改ignore文件

---
+++ webcode/autho2/src/main/java/com/wj/security/Autho2Application.java
@@ -0,0 +1,12 @@
+package com.wj.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autho2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Autho2Application.class, args);
+  }
+}
